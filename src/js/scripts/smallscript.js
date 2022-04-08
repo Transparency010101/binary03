356181e4c88c399d525a8539c02ad4f2e3f29440
Transparency010101 <ossys1010110@gmail.com>
2022-04-09 00:08:12 +0300

    сделал фильтер в portfolio

--- src/js/scripts/smallscript.js
+++ src/js/scripts/smallscript.js
@@ -1,12 +1,10 @@
 // * Этот файл для очень маленького кода
 // * для которого не стоит делать отдельный файл.
 
-// ! Измение свойсва flex для карточек для .about-cards в файле _about.scss(22ст)- {
-
+// * Измение свойсва flex для карточек для .about-cards в файле _about.scss(22ст)- {
 let aboutCardsColumn = document.querySelectorAll(".about-cards__column");
 
 let aboutCardsElementsCount = aboutCardsColumn.length;
-
 
 aboutCardsColumn.forEach(el => {
     switch (aboutCardsElementsCount) {
@@ -27,6 +25,45 @@ aboutCardsColumn.forEach(el => {
             break;
     }
 });
+// * Измение свойсва flex для карточек в about-cards- }
 
 
-// ! Измение свойсва flex для карточек в about-cards- }
+// * фильтр картинок в portfolio  {
+const portfolioDataFilterLink = document.querySelectorAll(".portfolio-data-filter__link");
+const portfolioGridColumn = document.querySelectorAll(".portfolio-grid__column");
+
+function toggleClassToFilter(filterClass) {
+    filterClass.classList.toggle("active");
+}
+
+portfolioDataFilterLink.forEach(el => {
+    el.addEventListener("click", (event) => {
+        // $ console.log(el.getAttribute("data-filter"));
+
+        portfolioGridColumn.forEach(classEl => {
+            if (el.getAttribute("data-filter") == 1) {
+                classEl.classList.remove("active");
+
+            } else if (el.getAttribute("data-filter") == 2) {
+
+                if (classEl.classList.contains("filter_2")) {
+                    toggleClassToFilter(classEl);
+                }
+
+            } else if (el.getAttribute("data-filter") == 3) {
+
+                if (classEl.classList.contains("filter_3")) {
+                    toggleClassToFilter(classEl);
+                }
+
+            } else if (el.getAttribute("data-filter") == 4) {
+
+                if (classEl.classList.contains("filter_4")) {
+                    toggleClassToFilter(classEl);
+                }
+            }
+        });
+        event.preventDefault();
+    });
+});
+// * фильтр для portfolio  }
